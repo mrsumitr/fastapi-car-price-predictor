7646006a60d07d6d0347d14cb75fa1596d51bb72
Sumit Ranjan <sumitran1234@gmail.com>
2025-12-23 11:49:31 +0530

    wrapping up the main file

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+from app.api import routes_auth, routes_predict
+from app.middleware.logging_middleware import LoggingMiddleware
+from app.core.exceptions import register_exception_handlers
+
+
+app=FastAPI(title='Car Price Prediction API')
+# link middlware
+app.add_middleware(LoggingMiddleware)
+#link endpoints
+app.include_router(routes_auth.router, tags=['Auth'])
+app.include_router(routes_predict.router, tags=['Prediction'])
+
+#monitoring through prometheus
+Instrumentator().instrument(app).expose(app)
+
+
+#add exception handler
+register_exception_handlers(app)
+
+ 
