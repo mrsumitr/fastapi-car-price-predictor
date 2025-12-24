5a9647f720d9a9cde20204be2bd47e0ba3283212
Sumit Ranjan <sumitran1234@gmail.com>
2025-12-24 22:01:38 +0530

    fixing bug

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -3,17 +3,13 @@ from app.core.config import settings
 from app.core.security import verify_token
 
 
-def get_api_key(api_key: str=Header(...)):
-  if api_key != settings.API_KEY:
-    raise HTTPException(status_code=403, detail='Invalid API Key')
+def get_api_key(api_key: str = Header(...)):
+    if api_key != settings.API_KEY:
+        raise HTTPException(status_code=403, detail='Invalid API Key')
+    
 
-
-def get_current_user(token: str =Header(...)):
-  payload= verify_token(token)
-  if not payload:
-    raise HTTPException(status_code=401, detail='Invalid JWT Token')
-  return payload
-
-
-
-
+def get_current_user(token: str = Header(...)):
+    payload = verify_token(token)
+    if not payload:
+        raise HTTPException(status_code=401, detail='Invalid JWT Token')
+    return payload
--- app/main.py
+++ app/main.py
@@ -4,19 +4,17 @@ from app.api import routes_auth, routes_predict
 from app.middleware.logging_middleware import LoggingMiddleware
 from app.core.exceptions import register_exception_handlers
 
+app = FastAPI(title='Car Price Prediction API')
 
-app=FastAPI(title='Car Price Prediction API')
-# link middlware
+# link middleware
 app.add_middleware(LoggingMiddleware)
-#link endpoints
+
+# link endpoints
 app.include_router(routes_auth.router, tags=['Auth'])
 app.include_router(routes_predict.router, tags=['Prediction'])
 
-#monitoring through prometheus
+# monitoring using Prometheus
 Instrumentator().instrument(app).expose(app)
 
-
-#add exception handler
+# add exception handler
 register_exception_handlers(app)
-
- 
